917f67f6932275e8355e6d5ed3cb150eefa33f8b
sunil-kumar <sunil@loadshare.net>
2022-04-03 23:40:36 +0530

    recording user info in firestore db while signing in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import {
     GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore';
+
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
@@ -18,3 +25,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    console.log("userDocRef", userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log("user snap shot is ", userSnapshot);
+
+    if(!userSnapshot.exists()){
+        const { displayName, email, photoURL } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                photoURL
+            });
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
+    }
+
+    return userDocRef;
+}
