42f2e4644ecff1803cca64a2422f4a9ddda926f4
sunil-kumar <sunil@loadshare.net>
2022-04-20 01:24:15 +0530

    added cartItem component and add item to cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
+                {
+                    cartItems.map((cartItem, index) => {
+                        return(
+                            <CartItem key={`${cartItem.id}-${index}`} cartItem={cartItem} />
+                        )
+                    })
+                }
                 <Button>Go to checkout</Button>
             </div>
         </div>
