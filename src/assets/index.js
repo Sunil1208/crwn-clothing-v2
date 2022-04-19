692693da811f6c6cb71675f6ebdfd93c07310fb6
sunil-kumar <sunil@loadshare.net>
2022-04-19 22:43:11 +0530

    Added cartIcon dummy component

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as ShoppingIcon } from './shopping-bag.svg';
+import { ReactComponent as CrwnLogo } from './crown.svg';
+
+export {
+    ShoppingIcon,
+    CrwnLogo,
+};
---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ShoppingIcon } from '../../assets';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+    return (
+        <div className='cart-icon-container'>
+            <ShoppingIcon className='shopping-icon' />
+            <span className='item-count'>0</span>
+        </div>
+    )
+}
+
+export default CartIcon;
