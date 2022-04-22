1036374d9482062c14712f14987d879412741fd8
sunil-kumar <sunil@loadshare.net>
2022-04-22 11:30:03 +0530

    removed classnames

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,8 +4,8 @@ import { FormInputLabel, Input, Group } from './form-input.styles';
 
 const FormInput = ({ label, ...otherProps}) => {
     return (
-        <Group className='group'>
-            <Input className='form-input' {...otherProps}/>
+        <Group>
+            <Input {...otherProps}/>
             {
                 label && (<FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>)
             }
