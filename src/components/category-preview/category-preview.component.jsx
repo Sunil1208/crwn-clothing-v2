532152d032a369c6a0020fad0bb4d1ab6ec956df
sunil-kumar <sunil@loadshare.net>
2022-04-22 21:47:27 +0530

    converted category preview to styled component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import ProductCard from '../product-card/product-card.component';
+import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles';
 
-import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
     const { pathname } = useLocation();
     return(
-        <div className='category-preview-container'>
+        <CategoryPreviewContainer>
             <h2>
                 <Link to={`${pathname}/${title}`}>
-                    <span className='title'>{title.toUpperCase()}</span>
+                    <Title>{title.toUpperCase()}</Title>
                 </Link>
             </h2>
-            <div className='preview'>
+            <Preview>
                 {
                     products.filter((_, idx) => idx < 4)
                         .map((product, index) => {
                             return(<ProductCard key={`${product.id}-${index}`} product={product} />)
                         })
                 }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     )
 };
 
