ecedc8da0854e7d8f6ea63f660a36b8d508fb581
sunil-kumar <sunil@loadshare.net>
2022-04-22 11:29:27 +0530

    converted from input to styled components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { FormInputLabel, Input, Group } from './form-input.styles';
 
-import './form-input.styles.scss';
 
 const FormInput = ({ label, ...otherProps}) => {
     return (
-        <div className='group'>
-            <input className='form-input' {...otherProps}/>
+        <Group className='group'>
+            <Input className='form-input' {...otherProps}/>
             {
-                label && (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>)
+                label && (<FormInputLabel shrink={otherProps.value.length}>{label}</FormInputLabel>)
             }
-        </div>
+        </Group>
     )
 }
 
