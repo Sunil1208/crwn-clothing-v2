e2a25fb36d5a241da71a279aed9731e685893c4f
sunil-kumar <sunil@loadshare.net>
2022-04-23 21:34:42 +0530

    optimized

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from 'react';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
@@ -80,21 +81,20 @@ export const CartProvider = ({ children }) => {
     const updateCartItemReducer = (newCartItems) => {
         const newCartCount = newCartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
         const newCartTotal = newCartItems.reduce((total, cartItem) => total + (cartItem.quantity * cartItem.price), 0);
-        dispatch({
-            type: CART_ACTION_TYPES.SET_CART_ITEMS,
-            payload: {
-                cartItems: newCartItems,
-                cartTotal: newCartTotal,
-                cartCount: newCartCount
-            }
-        });
+        dispatch(
+            createAction(
+                CART_ACTION_TYPES.SET_CART_ITEMS,
+                {
+                    cartItems: newCartItems,
+                    cartTotal: newCartTotal,
+                    cartCount: newCartCount
+                }
+            )
+            );
     };
 
     const setIsCartOpen = (bool) => {
-        dispatch({
-            type: CART_ACTION_TYPES.SET_IS_CART_OPEN,
-            payload: bool
-        });
+        dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool))
     };
 
     const addItemToCart = (productToAdd) => {
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 // as the actual value we want to access
 export const UserContext = createContext({
@@ -33,10 +34,7 @@ export const UserProvider = ({ children }) => {
     const { currentUser } = state;
 
     const setCurrentUser = (user) => {
-        dispatch({
-            type: USER_ACTION_TYPES.SET_CURRENT_USER,
-            payload: user
-        })
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
     }
     const value = { currentUser, setCurrentUser };
 
