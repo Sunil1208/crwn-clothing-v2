567a4e97ed7eff898dae9bfbd292ffa76efe6d03
sunil-kumar <sunil@loadshare.net>
2022-04-28 22:11:43 +0530

    added reselect library and use reselect in category selector

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,21 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducers';
 
+// CURRYING -> A fn that returns another fn;
+// const loggerMiddleware = (store) => (next) => (action) => {
+//     if(!action.type){
+//         return next(action);
+//     }
+
+//     console.log('type:', action.type);
+//     console.log('payload: ', action.payload);
+//     console.log('currentState: ', store.getState());
+
+//     next(action);
+
+//     console.log('nex state: ', store.getState());
+// }
+
 const middleWares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
