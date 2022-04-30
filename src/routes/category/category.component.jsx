04b26cb4b222d4516da3cc2ec9e4bb9b68eaa4da
sunil-kumar <sunil@loadshare.net>
2022-04-30 17:52:37 +0530

    showing loader on screen for an async api call

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
     const categoriesMap = useSelector(selectCategoriesMap);
     const { category } = useParams();
+    const isLoading = useSelector(selectCategoriesIsLoading);
+
 
     const [products, setProducts] = useState(categoriesMap[category]);
 
@@ -18,12 +21,18 @@ const Category = () => {
     return(
         <>
             <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-            <CategoryContainer>
             {
-                products && products.map((product) => {
-                    return(<ProductCard key={product.id} product={product} />)
-            })}
-            </CategoryContainer>
+                isLoading ? 
+                    (<Spinner />)
+                    : (
+                        <CategoryContainer>
+                        {
+                            products && products.map((product) => {
+                                return(<ProductCard key={product.id} product={product} />)
+                        })}
+                        </CategoryContainer>
+                    )
+            }
         </>
     )
 };
--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,7 @@ import { rootReducer } from './root-reducers';
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
